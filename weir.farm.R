f26c1519f37b8bb378aebbebc02c69a4586967bb
krmaas <kendra.r.maas@gmail.com>
2016-05-21 12:23:23 -0400

    Weir Farm Bioblitz Bacterial analysis
    Krona plots (interactive taxonomic charts http://bit.ly/1svZlbN )

---
+++ weir.farm.R
@@ -0,0 +1,16 @@
+### Weir Farm Bioblitz Bacteria
+
+
+# Read in data
+
+# Alpha diversity
+alpha <- read.csv(file="weir.farm.alpha.csv", header=T, )
+
+# Beta diversity
+tyc <- read.csv(file="weir.farm.tyc3.csv", header=T)
+jc <- read.csv(file="weir.farm.j3.csv", header=T)
+
+# Environmental data
+env <- read.csv(file="weir.farm.env.csv", header=T)
+
+
